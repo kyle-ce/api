84ebccce5cbc762ad14362f08e3e8ab19ca19c26
kyle-ce <xkcorc@gmail.com>
2024-04-07 13:33:47 -0400

    modularize code

--- src/payload.js
+++ src/payload.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const xml2js = require("xml2js");
 
 // Define the SOAP request payload
-const soapRequest = `
+const soapBody = `
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://example.com/webservice">
    <soapenv:Header/>
    <soapenv:Body>
@@ -14,32 +14,46 @@ const soapRequest = `
 </soapenv:Envelope>
 `;
 
-// Define the URL of your SOAP endpoint
+// Define the REST request payload
+const restBody = { arg1: 5, arg2: 10 };
+
+// Define the URL for SOAP and REST endpoints
 const SOAP_URL = "http://localhost:8000/soap";
 const REST_URL = "http://localhost:8001/rest";
 
 // Send the SOAP request using axios
-axios
-  .post(SOAP_URL, soapRequest, {
-    headers: {
-      "Content-Type": "text/xml",
-    },
-  })
-  .then((response) => {
-    // Parse the XML response to JavaScript object
-    xml2js.parseString(response.data, (err, result) => {
-      if (err) {
-        console.error("Error parsing SOAP response:", err);
-      } else {
-        // Handle the SOAP response
-        console.log(
-          "SOAP Response:",
-          result["soap:Envelope"]["soap:Body"][0]["tns:MyFunctionResponse"]
-        );
-      }
+const postSOAP = (body) => {
+  axios
+    .post(SOAP_URL, body, {
+      headers: {
+        "Content-Type": "text/xml",
+      },
+    })
+    .then((response) => {
+      // Parse the XML response to JavaScript object
+      xml2js.parseString(response.data, (err, result) => {
+        if (err) {
+          console.error("Error parsing SOAP response:", err);
+        } else {
+          // Handle the SOAP response
+          console.log(
+            "SOAP Response:",
+            result["soap:Envelope"]["soap:Body"][0]["tns:MyFunctionResponse"]
+          );
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error sending SOAP request:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error sending SOAP request:", error);
-  });
-axios.get(REST_URL).then(({ data }) => console.log("REST Response:", data));
+};
+// REST
+const postREST = (body) => {
+  axios
+    .post(REST_URL, body)
+    .then(({ data }) => console.log("REST Response:", data))
+    .catch((e) => console.error(e));
+};
+
+postSOAP(soapBody);
+postREST(restBody);
--- src/server.js
+++ src/server.js
@@ -7,34 +7,67 @@ const SOAP_PORT = 8000;
 const REST_PORT = 8001;
 const WSDL_PATH = "myservice.wsdl";
 
-const app = express();
-// Define a route for handling GET requests
-app.get("/rest", (req, res) => {
-  // Handle the GET request
-  res.json({ message: "This is a REST API response" });
-});
-
-const service = {
-  MyService: {
-    MyPort: {
-      MyFunction: (args) => ({
-        result: args.arg1 * args.arg2,
-      }),
-    },
-  },
+const myFunction = (args) => {
+  const { arg1, arg2 } = args;
+  return { result: arg1 * arg2 };
 };
 
-const xml = fs.readFileSync(WSDL_PATH, "utf8");
+// REST
+const startREST = () => {
+  const app = express();
+  app.use(express.json());
+  app.post("/rest", (req, res) => {
+    const args = req.body;
+    try {
+      res.json({ result: myFunction(args).result });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "An error occurred" });
+    }
+  });
+  app.listen(REST_PORT, () => {
+    console.log(`Server is running on http://localhost:${REST_PORT}/rest`);
+  });
+};
 
-const server = http.createServer((request, response) => {
-  response.statusCode = 404;
-  response.end("404: Not Found: " + request.url);
-});
+//SOAP
+const startSOAP = () => {
+  const service = {
+    MyService: {
+      MyPort: {
+        MyFunction: (args) => {
+          try {
+            return myFunction(args);
+          } catch (error) {
+            // Handle error
+            const fault = {
+              Fault: {
+                faultcode: "Server",
+                faultstring: "Internal Server Error",
+                detail: {
+                  errorMessage: error.message,
+                },
+              },
+            };
+            throw fault;
+          }
+        },
+      },
+    },
+  };
+  const xml = fs.readFileSync(WSDL_PATH, "utf8");
+  const server = http.createServer((request, response) => {
+    // Default server response before binding SOAP server below
+    response.statusCode = 404;
+    response.end("404: Not Found: " + request.url);
+  });
+  server.listen(SOAP_PORT, () =>
+    console.log(`Server is running on http://localhost:${SOAP_PORT}/soap`)
+  );
+  soap.listen(server, "/soap", service, xml, (err) => {
+    if (err) console.error("SOAP server errror:", err);
+  });
+};
 
-server.listen(SOAP_PORT, () =>
-  console.log(`Server is running on http://localhost:${SOAP_PORT}/soap`)
-);
-app.listen(REST_PORT, () => {
-  console.log(`Server is running on http://localhost:${REST_PORT}`);
-});
-soap.listen(server, "/soap", service, xml);
+startREST();
+startSOAP();
